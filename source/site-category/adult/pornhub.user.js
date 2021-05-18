29bc943acabd2cf3be34ad9da91cf51875b4ed4a
VisitAntarctica <84295609+VisitAntarctica@users.noreply.github.com>
2021-05-18 16:35:56 -0400

    Add auto-expand related videos

--- source/site-category/adult/pornhub.user.js
+++ source/site-category/adult/pornhub.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name     PornHub Tools
 // @include /^https://.*\.?pornhub(premium)?\.com/view_video.php/
-// @version  1.02
+// @version  1.03
 // @grant    none
 // @noframes
-// @description This includes some fixes/tools for PH UI
+// @description UX enhancements for PornHub power users
 // ==/UserScript==
 
 // Show all playlists when displaying the list
@@ -12,3 +12,6 @@
 var style = document.createElement('style');
 style.textContent = ".playlists div { height: max-content !important; max-height: none !important; }</style>";
 document.head.appendChild(style);
+
+// Show all related videos automatically
+document.getElementById('loadMoreRelatedVideosCenter').click();
