2b204fcbd9181f89c29dd8e9d0d92b8d98addd72
VisitAntarctica <84295609+VisitAntarctica@users.noreply.github.com>
2021-05-18 18:48:59 -0400

    Use a named function

--- source/site-category/adult/pornhub.user.js
+++ source/site-category/adult/pornhub.user.js
@@ -2,7 +2,7 @@
 // @name      PornHub Tools
 // @namespace /user-scripts/source/site-category/adult/pornhub.user.js 
 // @include /^https://.*\.?pornhub(premium)?\.com/view_video.php/
-// @version  1.04
+// @version  1.05
 // @grant    none
 // @noframes
 // @description UX enhancements for PornHub power users
@@ -15,9 +15,11 @@ style.textContent = ".playlists div { height: max-content !important; max-height
 document.head.appendChild(style);
 
 /* Run when DOM is fully loaded */ 
-document.addEventListener("DOMContentLoaded", (function(){
+function postrun(){
   console.log("DOMContentLoaded has fired!");
   // Show all related videos automatically
   document.getElementById('loadMoreRelatedVideosCenter').click();
   console.log("Post-load run is complete");
-  }));
+  document.removeEventListener(l);
+};
+var l = document.addEventListener("DOMContentLoaded", postrun);
