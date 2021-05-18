d7ccfcea9b8e793dda7a784d0d3325a836aab552
VisitAntarctica <84295609+VisitAntarctica@users.noreply.github.com>
2021-05-18 17:30:14 -0400

    Update pornhub.user.js

--- source/site-category/adult/pornhub.user.js
+++ source/site-category/adult/pornhub.user.js
@@ -15,9 +15,9 @@ style.textContent = ".playlists div { height: max-content !important; max-height
 document.head.appendChild(style);
 
 /* Run when DOM is fully loaded */ 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", (function(){
   console.log("DOMContentLoaded has fired!");
   // Show all related videos automatically
   document.getElementById('loadMoreRelatedVideosCenter').click();
   console.log("Post-load run is complete");
-});
+  }));
