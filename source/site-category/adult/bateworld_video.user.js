94487202ff59564068b97485e15b0d74ea4893d7
VisitAntarctica <84295609+VisitAntarctica@users.noreply.github.com>
2021-05-28 18:52:36 -0400

    Simplify script remove pasteboard function

--- source/site-category/adult/bateworld_video.user.js
+++ source/site-category/adult/bateworld_video.user.js
@@ -3,7 +3,7 @@
 // @namespace /user-scripts/source/site-category/adult/bateworld_video.user.js 
 // @include /^https://.*\.?bateworld?\.com/video.php/
 // @include /^https://.*\.?bateworld?\.com/profile.php/
-// @version  1.00
+// @version  1.01
 // @grant    none
 // @noframes
 // @description Video tools for Bateworld
@@ -41,18 +41,15 @@ function makeSourceLinks (){
         document.getElementsByClassName('page_header')[0].append(link);
 
         //jQuery('div.page_header').append('<a style="padding-left: 15px;" href="' + a.currentSrc + '" download="' + n + '">Download video</a>'); 
+        //GM.setClipboard(fileLabel);
     }
-    
 }
 
 /* Run when DOM is fully loaded */ 
 function postrun(){
     console.log("DOMContentLoaded has fired!");
-    // Show all related videos automatically
-    document.getElementById('loadMoreRelatedVideosCenter').click();
+    makeSourceLinks();
     console.log("Post-load run is complete");
     document.removeEventListener(l);
   };
 var l = document.addEventListener("DOMContentLoaded", postrun);
-  
-makeSourceLinks();
