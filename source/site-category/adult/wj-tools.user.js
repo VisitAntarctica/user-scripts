3867f13f2a815f7811a0cbbc47c4eefc5de3d718
VisitAntarctica <84295609+VisitAntarctica@users.noreply.github.com>
2021-08-01 10:20:06 -0400

    Use native methods

--- source/site-category/adult/wj-tools.user.js
+++ source/site-category/adult/wj-tools.user.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name     Webcamjackers Tools
 // @include /^https://.*\.?webcamjackers\.com/
-// @version  1.00
+// @version  1.01
 // @grant    none
 // @noframes
 // @description This includes some fixes/tools for WJ UI
 // ==/UserScript==
 
 // remove broken deceptive adlinks from video walls
-jQuery(jQuery.find('div.-video > a[href*="chaturbate"]')).each(function(i,e){ jQuery(e.parentNode).hide(); });
+// JQUERY
+//jQuery(jQuery.find('div.-video > a[href*="chaturbate"]')).each(function(i,e){ jQuery(e.parentNode).hide(); }); 
+// native
+document.querySelectorAll('div.-video > a[href*="chaturbate"]').forEach(function(e){e.parentNode.style.display="none"});
