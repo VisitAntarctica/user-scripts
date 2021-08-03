741b068dd76d8558064db673301f91864e7f3149
VisitAntarctica <84295609+VisitAntarctica@users.noreply.github.com>
2021-08-03 19:44:52 -0400

    Code cleanup

--- source/site-category/adult/wj-tools.user.js
+++ source/site-category/adult/wj-tools.user.js
@@ -2,17 +2,18 @@
 // @name     Webcamjackers Tools
 // @namespace /user-scripts/source/site-category/adult/wj-tools.user.js 
 // @include /^https://.*\.?webcamjackers\.com/
-// @version  1.03
+// @version  1.04
 // @grant    none
 // @noframes
 // @description This includes some fixes/tools for WJ UI
 // ==/UserScript==
 
 // remove broken deceptive adlinks from video walls
-// JQUERY
-//jQuery(jQuery.find('div.-video > a[href*="chaturbate"]')).each(function(i,e){ jQuery(e.parentNode).hide(); }); 
-// native
-document.querySelectorAll('div.-video > a[href*="chaturbate"]').forEach(function(e){e.parentNode.style.display="none"});
+document.querySelectorAll('div.-video > a[href*="chaturbate"]').forEach(
+    function(e){
+        e.parentNode.style.display = "none";
+    }
+);
 
 // remove the empty div keeping the video in a two-column
 document.querySelector('div.aside_spot').style.display = 'none';
