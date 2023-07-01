b9ebe8d25ac4c47db9f1c1c03088c9ae936db1d9
VisitAntarctica <84295609+VisitAntarctica@users.noreply.github.com>
2023-07-01 08:12:46 -0400

    Create userscript w/ initial features

---
+++ source/site-category/adult/lpsg_thread_video.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name      LPSG - Threaded video helper
+// @namespace /user-scripts/source/site-category/adult/lpsg_thread_video.user.js 
+// @include /^https://.*\.?lpsg?\.com/threads/.*
+// @version  1.00
+// @grant    none
+// @noframes
+// @description Helper for videos in threads on LPSG
+// ==/UserScript==
+
+var do_work = () => {
+    var posters = document.querySelectorAll("div.video-easter-egg-poster");
+    for( var i = 0 ; i < posters.length ; i++ ){
+        var poster = posters[i].querySelector("img");
+        
+        var video_element = make_video(poster);
+        
+        var poster_container = poster.parentElement.parentElement;
+        if( video_element !== null ){
+            poster_container.appendChild(video_element);
+            poster.remove();
+        }   
+    }
+};
+
+var make_src_url = (url_key) => {
+    url_key = url_key || "";
+    if( url_key.length == 0 ) return;   
+    
+    return "https://cdn-videos.lpsg.com/data/video/" + url_key + ".mov";
+};
+
+var make_video = (poster_el) => {
+    var poster_url = poster_el.getAttribute('src');
+    if( poster_url.length == 0 ){
+        return;
+    }
+    var vid = document.createElement('video');
+    vid.setAttribute('controls' , 'true');
+    vid.setAttribute('data-poster', poster_url);
+    vid.setAttribute('poster' , poster_url);
+    vid.setAttribute('data-xf-init', 'video-init');
+    
+    var matches = [...poster_url.matchAll(/\/posters\/(.*?)(\..*)$/ig)];
+    var srcKey = matches[0][1] || "";
+    
+    if( srcKey.length == 0 ) return;
+    
+    var srcUrl = make_src_url(srcKey);
+    if( srcUrl.length == 0 ) return; 
+    
+    var src = document.createElement('source');
+    src.setAttribute('data-src' , srcUrl);
+    src.setAttribute('src' , srcUrl);
+    
+    vid.appendChild(src);
+    return vid;
+}
+
+(function() {
+    do_work();
+})();
