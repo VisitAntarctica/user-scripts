87d9e1eca47fae6b6f3268ccc4f43710873a8935
VisitAntarctica <84295609+VisitAntarctica@users.noreply.github.com>
2024-07-13 08:53:19 -0400

    Update CDN root; v1.35

--- source/site-category/adult/bateworld_video.user.js
+++ source/site-category/adult/bateworld_video.user.js
@@ -4,7 +4,7 @@
 // @include /^https://.*\.?bateworld?\.com/(bate[\w\d\-_]+?)?video(_group|album)?.php/
 // @include /^https://.*\.?bateworld?\.com/profile.php/
 // @include /^https://.*\.?bateworld?\.com/bator_training.*/
-// @version  1.34
+// @version  1.35
 // @grant    none
 // @noframes
 // @description Video tools for Bateworld
@@ -46,7 +46,7 @@ var aspects = [
     { key: "720" },
     { key: "480" }
 ];
-var CDN_ROOT = 'https://cdn.bateworld.com/';
+var CDN_ROOT = 'https://cloudcdn.bateworld.com/';
 var VID_EXTENSION = '.mp4';
 var VIDEO_INDEX_BREAKPOINT = 101164;
 
