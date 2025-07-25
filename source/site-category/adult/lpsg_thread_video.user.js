bf7a269727edbef40f4a90f1ea50a9c1aa31d3aa
VisitAntarctica <84295609+VisitAntarctica@users.noreply.github.com>
2025-07-25 16:16:43 -0400

    Remove custom download label; v1.20

--- source/site-category/adult/lpsg_thread_video.user.js
+++ source/site-category/adult/lpsg_thread_video.user.js
@@ -3,7 +3,7 @@
 // @namespace /user-scripts/source/site-category/adult/lpsg_thread_video.user.js 
 // @include /^https://.*\.?lpsg?\.com/threads/.*/
 // @include /^https://.*\.?lpsg?\.com/gallery/.*/
-// @version  1.19
+// @version  1.20
 // @grant    none
 // @noframes
 // @description Helper for videos in threads on LPSG
@@ -121,7 +121,7 @@ var btn_success = ( el ) => {
 
     // try and find a title in the post
     var downloadTag = '';
-    try {
+    /*try {
         var topContainer = el.parentElement.parentElement;
         // first try: previous node
         
@@ -137,7 +137,7 @@ var btn_success = ( el ) => {
         }
     } catch(e){
         console.log(e);
-    }
+    }*/
     
     // make the link to the source
     var href = document.createElement('a');
