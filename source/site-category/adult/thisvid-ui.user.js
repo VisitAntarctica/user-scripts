020172d4ee412529e62c5578a0fd30a3a44e5dbe
VisitAntarctica <84295609+VisitAntarctica@users.noreply.github.com>
2025-07-30 08:49:25 -0400

    Show IDs for video on page (if one is present); v1.12

--- source/site-category/adult/thisvid-ui.user.js
+++ source/site-category/adult/thisvid-ui.user.js
@@ -2,7 +2,7 @@
 // @name      Thisvid - UI fixes
 // @namespace /user-scripts/source/site-category/adult/thisvid-ui.user.js
 // @include /^https://.*\.?thisvid?\.com/.*/
-// @version  1.10
+// @version  1.12
 // @grant    none
 // @noframes
 // @description Thisvid UI enhancements
@@ -12,6 +12,8 @@
 // Configuration
 var idElementType = 'span';
 var idElementClass = 'this-id';
+var thumbRE = new RegExp(/\/(\d+)\/[^/]*?\/\d+\.\w{3,4}$/ig);
+var posterRE = new RegExp(/\/(\d+)\/[^/]*?\/[\w\d.]+\.\w{3,4}$/ig);
 
 // Function definition
 var colorPercent = function( percentStr ){
@@ -47,6 +49,72 @@ var colorPercent = function( percentStr ){
         r["background-color"] = "#a80037"; // dark red
     }
     return r;
+};
+
+// parse imgID out of image elements
+var exposeImgId = ( img , appendEl , regExp ) => {
+    appendEl = appendEl || false; 
+    var par = null;
+    if( appendEl === false ){
+        console.warn('No append element provided, bailing out!');
+        return;
+    } else {
+        par = appendEl;
+    }
+    if( regExp === false ){
+        console.warn('No regex provided, bailing out!');
+        return;
+    }
+    // gather the ID from a thumb image
+    // sometimes the src is stored in data-original and a placeholder base64 encoded image is stored in the img src
+    var src = ( img.src.substr(0,5) == 'data:' ) ? img.getAttribute('data-original') : img.src;
+    var mArr = [...src.matchAll( regExp )];
+    var imgId = null;
+    if( mArr.length > 0 && mArr[0].length > 1 ){
+        imgId = mArr[0][1];
+    }
+    var idParts = imgId !== null ? [...imgId.matchAll(/^(\d{0,4})(\d*)(\d{4})$/g)] : [ null ];
+    if( imgId !== null ){
+        // get the wrapper
+        // var par =;
+
+        // if there is a span with ID already, get and use it
+        var existEl = par.querySelector(`${idElementType}.${idElementClass}`);
+
+        par.appendChild( 
+            makeIdElement( imgId , idParts, existEl )
+        );
+    }
+}
+
+// make and return the ID display element
+var makeIdElement = ( imgId , idParts , existingElement ) => {
+    var existEl = existingElement || false; 
+    var idElement = null;
+    if( existEl !== false ){
+        // yes, clear the element 
+        existEl.innerHTML = '';
+        idElement = existEl;
+    } else {
+        // no, create the element
+        idElement = document.createElement( idElementType );
+    }
+    // make the inner parts and add them to the idElement
+    idElement.setAttribute( 'class' , idElementClass );
+    idElement.setAttribute('title' , imgId );
+    idElement.setAttribute('onclick' , 'javascript: return false;')
+    if( idParts.length > 0 && idParts[0].length > 1 ){
+        var p = idParts[0].shift();
+        do {
+            p = idParts[0].shift();
+            var pEl = document.createElement( 'span' );
+            pEl.innerText = p;
+            idElement.appendChild( pEl );
+        } while( idParts[0].length > 0 );
+    } else {
+        idElement.innerText = imgId;
+    }
+    return idElement;
 };
 
 (function(){
@@ -88,52 +156,15 @@ ${idElementType}.${idElementClass} span:last-of-type {
         el.setAttribute('style' , `background-color: ${unit["background-color"]}; color: ${unit.color}; font-weight: ${unit["font-weight"]}`);
     });
     // FUNCTION: Float the video ID into view
-    var mvrex = new RegExp(/\/(\d+)\/[^/]*?\/\d+\.\w{3,4}$/ig);
+
     var thimg = document.querySelectorAll('span.thumb > img');
     thimg.forEach((img) => {
-        // gather the ID from a thumb image
-        // sometimes the src is stored in data-original and a placeholder base64 encoded image is stored in the img src
-        var src = ( img.src.substr(0,5) == 'data:' ) ? img.getAttribute('data-original') : img.src;
-        var mArr = [...src.matchAll( mvrex )];
-        var imgId = null;
-        if( mArr.length > 0 && mArr[0].length > 1 ){
-            imgId = mArr[0][1];
-        }
-        var idParts = imgId !== null ? [...imgId.matchAll(/^(\d{0,4})(\d*)(\d{4})$/g)] : [ null ];
-        if( imgId !== null ){
-            // get the wrapper
-            var par = img.parentElement.parentElement;
-
-            // is there a span with ID already?
-            var idElement = null;
-            var existEl = par.querySelector(`${idElementType}.${idElementClass}`);
-            if( existEl !== null ){
-                // yes, clear the element 
-                existEl.innerHTML = '';
-                idElement = existEl;
-            } else {
-                // no, create the element
-                idElement = document.createElement( idElementType );
-            }
-            // make the inner parts and add them to the idElement
-            idElement.setAttribute( 'class' , idElementClass );
-            idElement.setAttribute('title' , imgId );
-            idElement.setAttribute('onclick' , 'javascript: return false;')
-            if( idParts.length > 0 && idParts[0].length > 1 ){
-                var imgIdMatch = ( idParts[0].shift() == imgId ); 
-                do {
-                    var p = idParts[0].shift();
-                    var pEl = document.createElement( 'span' );
-                    pEl.innerText = p;
-                    idElement.appendChild( pEl );
-                } while( idParts[0].length > 0 );
-            } else {
-                idElement.innerText = imgId;
-            }
-            par.appendChild( idElement );
-        }
+        exposeImgId( img , img.parentElement.parentElement , thumbRE);
     });
-
-    
+    var vimg = document.querySelectorAll('.fp-poster > img');
+    vimg.forEach((img) => {
+        debugger;
+        exposeImgId( img , document.querySelector('div.headline') , posterRE);
+    });
 })();
 
